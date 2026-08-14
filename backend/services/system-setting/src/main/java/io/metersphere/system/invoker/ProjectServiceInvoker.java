06a3269ddbc7b5648bc45ec93535918c0a810c0a
chenqifen-miduo <chenqf@miduonet.com>
2026-08-14 21:58:27 +0800

    fix: break project service circular dependency

--- backend/services/system-setting/src/main/java/io/metersphere/system/invoker/ProjectServiceInvoker.java
+++ backend/services/system-setting/src/main/java/io/metersphere/system/invoker/ProjectServiceInvoker.java
@@ -3,32 +3,30 @@ package io.metersphere.system.invoker;
 import io.metersphere.system.service.CleanupProjectResourceService;
 import io.metersphere.system.service.CreateProjectResourceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public class ProjectServiceInvoker {
-    private final List<CleanupProjectResourceService> cleanupProjectResourceServices;
+    private final ObjectProvider<CleanupProjectResourceService> cleanupProjectResourceServices;
 
-    private final List<CreateProjectResourceService> createProjectResourceServices;
+    private final ObjectProvider<CreateProjectResourceService> createProjectResourceServices;
 
 
     @Autowired
-    public ProjectServiceInvoker(List<CleanupProjectResourceService> services, List<CreateProjectResourceService> createProjectResourceServices) {
+    public ProjectServiceInvoker(ObjectProvider<CleanupProjectResourceService> services,
+                                 ObjectProvider<CreateProjectResourceService> createProjectResourceServices) {
         this.cleanupProjectResourceServices = services;
         this.createProjectResourceServices = createProjectResourceServices;
     }
 
     public void invokeServices(String projectId) {
-        for (CleanupProjectResourceService service : cleanupProjectResourceServices) {
-            service.deleteResources(projectId);
-        }
+        cleanupProjectResourceServices.orderedStream()
+                .forEach(service -> service.deleteResources(projectId));
     }
 
     public void invokeCreateServices(String projectId) {
-        for (CreateProjectResourceService service : createProjectResourceServices) {
-            service.createResources(projectId);
-        }
+        createProjectResourceServices.orderedStream()
+                .forEach(service -> service.createResources(projectId));
     }
 }
